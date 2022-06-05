1e6dc4d7a29f48692b978a889786381fbcc4ca8c
Michael Mahoney <minthammockdevelopment@gmail.com>
2022-06-05 08:57:45 -0600

    Changing boot loader to account for BIOS parameter block.

--- boot.asm
+++ boot.asm
@@ -1,7 +1,15 @@
 ORG 0
 BITS 16
 
+_start:
+    jmp short start
+    nop
+times 33 db 0
+jmp 0x7c0:start
+
 start:
+    jmp 0x7c0:step2
+step2:
     cli ;clear interrupts
     mov ax, 0x7c0
     mov ds, ax
@@ -10,6 +18,7 @@ start:
     mov ss, ax
     mov sp, 0x7c00
     sti ; enables interrupts
+
     mov si, message
     call print
     jmp $
